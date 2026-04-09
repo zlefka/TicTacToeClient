f5cc9b93d8f1733f1879f064530e41445872a9de
Lera Udovenchuk <leraudovenchuk@icloud.com>
2026-04-09 18:20:53 +0300

    create game fragment added

--- app/src/main/java/com/example/tictactoemobile/presentation/ui/CreateGameFragment.kt
+++ app/src/main/java/com/example/tictactoemobile/presentation/ui/CreateGameFragment.kt
@@ -1,25 +1,90 @@
 package com.example.tictactoemobile.presentation.ui
 
 import android.os.Bundle
+import android.text.Editable.Factory
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.example.tic_tac_toe_mobile.R
 import com.example.tic_tac_toe_mobile.databinding.FragmentCreateGameBinding
 import com.example.tictactoemobile.presentation.viewmodel.CreateGameFragmentViewModel
+import com.example.tictactoemobile.presentation.viewmodel.CreateGameFragmentViewModelFactory
+import com.example.tictactoemobile.presentation.viewmodel.StatesOfCreatingGames
+import com.example.tictactoemobile.presentation.viewmodel.StatesOfLogin
 
 class CreateGameFragment : Fragment() {
     private var _binding: FragmentCreateGameBinding? = null
     private val binding get() = _binding!!
-    lateinit var viewModel: CreateGameFragmentViewModel
+    private lateinit var viewModel: CreateGameFragmentViewModel
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_game, container, false)
+        _binding = FragmentCreateGameBinding.inflate(inflater, container, false)
+        val view = binding.root
+        val repository = (activity as MainActivity).appComponent.getGameRepository()
+        val factory = CreateGameFragmentViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, factory).get(CreateGameFragmentViewModel::class.java)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.state.observe(viewLifecycleOwner, Observer { newValue ->
+            when (newValue) {
+                StatesOfCreatingGames.Loading -> binding.progressBar.isVisible = true
+                is StatesOfCreatingGames.Success -> {
+                    binding.progressBar.isVisible = false
+                    Toast.makeText(activity, "Game created!", Toast.LENGTH_SHORT).show()
+                    val action = CreateGameFragmentDirections.actionCreateGameFragmentToCurrentGameFragment()
+                    view.findNavController().navigate(action)
+                }
+                is StatesOfCreatingGames.Error -> {
+                    binding.progressBar.isVisible = false
+                    Toast.makeText(activity, newValue.reason, Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+        binding.tvToAvailableGames.setOnClickListener {
+            val action = CreateGameFragmentDirections.actionCreateGameFragmentToExistingGamesFragment()
+            view.findNavController().navigate(action)
+        }
+
+        binding.buttonCreateGame.setOnClickListener {
+            val symbol = when{
+                binding.chipO.isChecked -> "O"
+                binding.chipX.isChecked -> "X"
+                else -> null
+            }
+            if(symbol == null) {
+                Toast.makeText(requireContext(), "Choose your symbol", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val isBotSelected = binding.chipBot.isChecked || binding.chipTwoPlayers.isChecked
+            if(!isBotSelected) {
+                Toast.makeText(requireContext(), "Choose game mode", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val isBot = binding.chipBot.isChecked
+
+            viewModel.createGame(symbol, isBot)
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/tictactoemobile/presentation/viewmodel/CreateGameFragmentViewModel.kt
+++ app/src/main/java/com/example/tictactoemobile/presentation/viewmodel/CreateGameFragmentViewModel.kt
@@ -1,8 +1,35 @@
 package com.example.tictactoemobile.presentation.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.tictactoemobile.data.repository.GameRepository
+import com.example.tictactoemobile.presentation.mapper.toViewData
+import kotlinx.coroutines.launch
 
-class CreateGameFragmentViewModel(repository: GameRepository): ViewModel() {
+class CreateGameFragmentViewModel(private val repository: GameRepository): ViewModel() {
+    private val _error = MutableLiveData<String>()
+    val error: LiveData<String> get() = _error
 
+    private val _state = MutableLiveData<StatesOfCreatingGames>()
+    val state: LiveData<StatesOfCreatingGames> get() = _state
+
+    fun createGame(playerSymbol: String, isBot: Boolean) {
+        _state.value = StatesOfCreatingGames.Loading
+        viewModelScope.launch {
+            try {
+                val game = repository.createGame(playerSymbol, isBot)
+                Log.i("create game", "request was sent")
+                if(game != null) {
+                    Log.i("create game", "the game isn't null")
+                    val viewData = game.toViewData()
+                    _state.value = StatesOfCreatingGames.Success(viewData)
+                } else _state.value = StatesOfCreatingGames.Error("Can't create game")
+            } catch (e: Exception) {
+                _state.value = StatesOfCreatingGames.Error(e.message ?: "Unknown error")
+            }
+        }
+    }
 }
